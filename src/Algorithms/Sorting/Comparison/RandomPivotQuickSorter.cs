8f3ff9c76d902b978b221dd7f53ec0ea28c59c84
Alexander Shavrov <cyber.crate1@gmail.com>
2022-01-18 15:49:24 +0300

    Implemented random pivot quick sort algorithm

---
+++ src/Algorithms/Sorting/Comparison/RandomPivotQuickSorter.cs
@@ -0,0 +1,12 @@
+﻿namespace Algorithms.Sorting.Comparison;
+
+/// <summary>
+/// Sorts arrays using quicksort (selecting random point as a pivot).
+/// </summary>
+public sealed class RandomPivotQuickSorter : QuickSorter
+{
+    private readonly Random _random = new();
+
+    protected override T SelectPivot<T>(T[] array, IComparer<T> comparer, int left, int right) =>
+        array[_random.Next(left, right + 1)];
+}
---
+++ tests/Algorithms.Tests/Sorting/Comparison/RandomPivotQuickSorterTests.cs
@@ -0,0 +1,23 @@
+﻿using Algorithms.Sorting.Comparison;
+using Algorithms.Tests.Helpers;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Sorting.Comparison;
+
+public static class RandomPivotQuickSorterTests
+{
+    [Test]
+    public static void ArraySorted([Random(0, 1000, 100, Distinct = true)] int n)
+    {
+        // Arrange.
+        IComparisonSorter sorter = new RandomPivotQuickSorter();
+        var (correctArray, testArray) = RandomHelper.GetArrays(n);
+
+        // Act.
+        sorter.Sort(testArray, new IntegralComparer());
+        Array.Sort(correctArray);
+
+        // Assert.
+        Assert.AreEqual(testArray, correctArray);
+    }
+}
