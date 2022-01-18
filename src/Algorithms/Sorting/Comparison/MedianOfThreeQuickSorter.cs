e9028b269931d094be9e0fdb94128bc060b4e55f
Alexander Shavrov <cyber.crate1@gmail.com>
2022-01-18 16:08:50 +0300

    Implemented median of three quick sort algorithm

---
+++ src/Algorithms/Sorting/Comparison/MedianOfThreeQuickSorter.cs
@@ -0,0 +1,36 @@
+﻿namespace Algorithms.Sorting.Comparison;
+
+/// <summary>
+/// Sorts arrays using quicksort (selecting median of three as a pivot).
+/// </summary>
+public class MedianOfThreeQuickSorter : QuickSorter
+{
+    protected override T SelectPivot<T>(T[] array, IComparer<T> comparer, int left, int right)
+    {
+        var leftPoint = array[left];
+        var middlePoint = array[left + (right - left) / 2];
+        var rightPoint = array[right];
+
+        return FindMedian(comparer, leftPoint, middlePoint, rightPoint);
+    }
+    
+    private static T FindMedian<T>(IComparer<T> comparer, T a, T b, T c)
+    {
+        if (comparer.Compare(a, b) <= 0)
+        {
+            if (comparer.Compare(b, c) <= 0)
+            {
+                return b;
+            }
+
+            return comparer.Compare(a, c) <= 0 ? c : a;
+        }
+
+        if (comparer.Compare(b, c) >= 0)
+        {
+            return b;
+        }
+
+        return comparer.Compare(a, c) >= 0 ? c : a;
+    }
+}
---
+++ tests/Algorithms.Tests/Sorting/Comparison/MedianOfThreeQuickSorterTests.cs
@@ -0,0 +1,23 @@
+﻿using Algorithms.Sorting.Comparison;
+using Algorithms.Tests.Helpers;
+using NUnit.Framework;
+
+namespace Algorithms.Tests.Sorting.Comparison;
+
+public static class MedianOfThreeQuickSorterTests
+{
+    [Test]
+    public static void SortArrays([Random(0, 1_000, 100, Distinct = true)] int n)
+    {
+        // Arrange.
+        IComparisonSorter sorter = new MedianOfThreeQuickSorter();
+        var (correctArray, testArray) = RandomHelper.GetArrays(n);
+
+        // Act.
+        sorter.Sort(testArray, new IntegralComparer());
+        Array.Sort(correctArray);
+
+        // Assert.
+        Assert.AreEqual(testArray, correctArray);
+    }
+}
