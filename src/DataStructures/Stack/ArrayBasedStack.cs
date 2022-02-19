e863e774bbc8eea38c9899a1e68bff69ad1f4b21
Alexander Shavrov <alexander.shavrov@aspose.com>
2022-02-19 15:37:45 +0300

    Implemented array based stack class

---
+++ src/DataStructures/Stack/ArrayBasedStack.cs
@@ -0,0 +1,113 @@
+﻿namespace DataStructures.Stack;
+
+/// <summary>
+/// Implements array-based stack with LIFO style.
+/// </summary>
+public class ArrayBasedStack<T>
+{
+    private const int DefaultCapacity = 10;
+
+    /// <summary>
+    /// <see cref="Array" /> based stack.
+    /// </summary>
+    private T[] _stack;
+
+    /// <summary>
+    /// Gets the number of elements on the <see cref="ArrayBasedStack{T}" />.
+    /// </summary>
+    public int Top { get; private set; }
+
+    /// <summary>
+    /// Gets or sets the Capacity of the <see cref="ArrayBasedStack{T}" />.
+    /// </summary>
+    public int Capacity
+    {
+        get => _stack.Length;
+        set => Array.Resize(ref _stack, value);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArrayBasedStack{T}" /> class.
+    /// </summary>
+    public ArrayBasedStack()
+    {
+        _stack = new T[DefaultCapacity];
+        Top = -1;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArrayBasedStack{T}" /> class.
+    /// </summary>
+    /// <param name="item">Item to push onto the <see cref="ArrayBasedStack{T}" />.</param>
+    public ArrayBasedStack(T item) : this() => Push(item);
+    
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArrayBasedStack{T}" /> class.
+    /// </summary>
+    /// <param name="items">Items to push onto the <see cref="ArrayBasedStack{T}" />.</param>
+    public ArrayBasedStack(IEnumerable<T> items) : this()
+    {
+        foreach (var item in items)
+        {
+            Push(item);
+        }
+    }
+
+    /// <summary>
+    /// Inserts an item at the top of the <see cref="ArrayBasedStack{T}" />.
+    /// </summary>
+    /// <param name="item">The item to push onto the <see cref="ArrayBasedStack{T}" />.</param>
+    public void Push(T item)
+    {
+        if (Top == Capacity - 1)
+        {
+            Capacity *= 2;
+        }
+
+        _stack[++Top] = item;
+    }
+    
+    /// <summary>
+    /// Removes and returns the item at the top of the <see cref="ArrayBasedStack{T}" />.
+    /// </summary>
+    /// <returns>The item removed from the top of the <see cref="ArrayBasedStack{T}" />.</returns>
+    public T Pop()
+    {
+        if (Top == -1)
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+
+        return _stack[Top--];
+    }
+    
+    /// <summary>
+    /// Returns the item at the top of the <see cref="ArrayBasedStack{T}" /> without removing it.
+    /// </summary>
+    /// <returns>The item at the top of the <see cref="ArrayBasedStack{T}" />.</returns>
+    public T Peek()
+    {
+        if (Top == -1)
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+
+        return _stack[Top];
+    }
+    
+    /// <summary>
+    /// Determines whether an element is in the <see cref="ArrayBasedStack{T}" />.
+    /// </summary>
+    /// <param name="item">The item to locate in the <see cref="ArrayBasedStack{T}" />.</param>
+    /// <returns>True, if the item is in the stack.</returns>
+    public bool Contains(T item) => Array.IndexOf(_stack, item, 0, Top + 1) > -1;
+    
+    /// <summary>
+    /// Removes all items from the <see cref="ArrayBasedStack{T}" />.
+    /// </summary>
+    public void Clear()
+    {
+        Top = -1;
+        Capacity = DefaultCapacity;
+    }
+}
---
+++ tests/DataStructures.Tests/Stack/ArrayBasedStackTests.cs
@@ -0,0 +1,120 @@
+﻿using DataStructures.Stack;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DataStructures.Tests.Stack;
+
+public static class ArrayBasedStackTests
+{
+    [Test]
+    public static void CountTest() => new ArrayBasedStack<int>(new[] { 0, 1, 2, 3, 4 }).Top.Should().Be(4);
+
+    [Test]
+    public static void ClearTest()
+    {
+        ArrayBasedStack<int> stack = new(new[] {0, 1, 2, 3, 4});
+        stack.Clear();
+        stack.Top.Should().Be(-1);
+    }
+
+    [Test]
+    public static void ContainsTest()
+    {
+        ArrayBasedStack<int> stack = new(new[] { 0, 1, 2, 3, 4 });
+
+        Assert.Multiple(() =>
+        {
+            stack.Contains(0).Should().BeTrue();
+            stack.Contains(1).Should().BeTrue();
+            stack.Contains(2).Should().BeTrue();
+            stack.Contains(3).Should().BeTrue();
+            stack.Contains(4).Should().BeTrue();
+        });
+    }
+
+    [Test]
+    public static void PeekTest()
+    {
+        ArrayBasedStack<int> stack = new(new[] { 0, 1, 2, 3, 4 });
+
+        Assert.Multiple(() =>
+        {
+            stack.Peek().Should().Be(4);
+            stack.Peek().Should().Be(4);
+            stack.Peek().Should().Be(4);
+        });
+    }
+
+    [Test]
+    public static void PopTest()
+    {
+        ArrayBasedStack<int> stack = new(new[] { 0, 1, 2, 3, 4 });
+
+        Assert.Multiple(() =>
+        {
+            stack.Pop().Should().Be(4);
+            stack.Pop().Should().Be(3);
+            stack.Pop().Should().Be(2);
+            stack.Pop().Should().Be(1);
+            stack.Pop().Should().Be(0);
+        });
+    }
+
+    [Test]
+    public static void PushTest()
+    {
+        ArrayBasedStack<int> stack = new();
+
+        Assert.Multiple(() =>
+            Enumerable
+                .Range(0, 5)
+                .ToList()
+                .ForEach(number =>
+                {
+                    stack.Push(number);
+                    stack.Peek().Should().Be(number);
+                }));
+    }
+
+    [Test]
+    public static void AutomaticResizesTest()
+    {
+        const int initialCapacity = 2;
+        
+        ArrayBasedStack<int> stack = new() { Capacity = initialCapacity };
+        stack.Push(0);
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        stack.Push(4);
+
+        stack.Capacity.Should().BeGreaterThan(initialCapacity);
+    }
+
+    [Test]
+    public static void ShouldThrowStackEmptyExceptionOnEmptyPopTest()
+    {
+        ArrayBasedStack<int> stack = new();
+
+        Action poppingAnEmptyStack = () => stack.Pop();
+
+        poppingAnEmptyStack
+            .Should()
+            .Throw<InvalidOperationException>()
+            .WithMessage("Stack is empty");
+
+    }
+
+    [Test]
+    public static void ShouldThrowStackEmptyExceptionOnEmptyPeekTest()
+    {
+        ArrayBasedStack<int> stack = new();
+        
+        Action peekingAnEmptyStack = () => stack.Peek();
+        
+        peekingAnEmptyStack
+            .Should()
+            .Throw<InvalidOperationException>()
+            .WithMessage("Stack is empty");
+    }
+}
