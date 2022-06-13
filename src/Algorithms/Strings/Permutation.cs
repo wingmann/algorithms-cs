88ecb3fe6298a2d168186bc3b18eedbec57b3b63
Alexander Shavrov <alexander.shavrov@outlook.com>
2022-06-13 07:55:28 +0300

    Implemented string permutation algorithm

---
+++ src/Algorithms/Strings/Permutation.cs
@@ -0,0 +1,33 @@
+﻿namespace Wingmann.Algorithms.Strings;
+
+/// <summary>
+/// Implements string permutation algorithm.
+/// See on <see href="https://en.wikipedia.org/wiki/Permutation">Wikipedia</see>.
+/// </summary>
+public static class Permutation
+{
+    /// <summary>
+    /// Collects list of all the permutations of <paramref name="word" />.
+    /// </summary>
+    /// <param name="word">Input string value.</param>
+    /// <returns>List of all the permutations of <paramref name="word" />.</returns>
+    public static List<string> GetEveryUniquePermutation(string word)
+    {
+        if (word.Length < 2)
+        {
+            return new List<string> { word };
+        }
+
+        HashSet<string> result = new();
+
+        for (var i = 0; i < word.Length; i++)
+        {
+            result.UnionWith(
+                GetEveryUniquePermutation(word.Remove(i, 1))
+                    .Select(subPerm => word[i] + subPerm)
+            );
+        }
+
+        return result.ToList();
+    }
+}
---
+++ tests/Algorithms.Tests/Strings/PermutationTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using Wingmann.Algorithms.Numeric;
+using Wingmann.Algorithms.Strings;
+
+namespace Wingmann.Algorithms.Tests.Strings;
+
+public static class PermutationTests
+{
+    [TestCase("")]
+    [TestCase("A")]
+    [TestCase("abcd")]
+    [TestCase("aabcd")]
+    [TestCase("aabbbcd")]
+    [TestCase("aabbbccccd")]
+    public static void Test_GetEveryUniquePermutation(string word)
+    {
+        var permutations = Permutation.GetEveryUniquePermutation(word);
+        Dictionary<char, int> occurrence = new();
+
+        foreach (var c in word)
+        {
+            if (occurrence.ContainsKey(c))
+            {
+                occurrence[c] += 1;
+            }
+            else
+            {
+                occurrence[c] = 1;
+            }
+        }
+        
+        var expectedNumberOfAnagrams = Factorial.Calculate(word.Length);
+        
+        expectedNumberOfAnagrams = occurrence.Aggregate(
+            expectedNumberOfAnagrams,
+            (current, keyValuePair) => current / Factorial.Calculate(keyValuePair.Value));
+        
+        Assert.AreEqual(expectedNumberOfAnagrams, permutations.Count);
+
+        var wordSorted = SortString(word);
+        
+        foreach (var permutation in permutations)
+        {
+            Assert.AreEqual(wordSorted, SortString(permutation));
+        }
+
+        Assert.AreEqual(permutations.Count, new HashSet<string>(permutations).Count);
+    }
+
+    private static string SortString(string word)
+    {
+        var asArray = word.ToArray();
+        Array.Sort(asArray);
+        return new string(asArray);
+    }
+}
