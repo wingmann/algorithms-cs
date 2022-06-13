a5fa5bf2ac91a110c8c0467749a924e9156a06a8
Alexander Shavrov <alexander.shavrov@outlook.com>
2022-06-13 07:54:24 +0300

    Implemented factorial calculation algorithm

---
+++ src/Algorithms/Numeric/Factorial.cs
@@ -0,0 +1,23 @@
+﻿namespace Wingmann.Algorithms.Numeric;
+
+/// <summary>
+/// Implements factorial algorithm.
+/// See on <see href="https://en.wikipedia.org/wiki/Factorial">Wikipedia</see>
+/// </summary>
+public static class Factorial
+{
+    /// <summary>
+    /// Calculates factorial of a number.
+    /// </summary>
+    /// <param name="number">Input number.</param>
+    /// <returns>Factorial of input number.</returns>
+    public static long Calculate(int number)
+    {
+        if (number < 0)
+        {
+            throw new ArgumentException("Only for numbers equal to or greater than 0");
+        }
+        
+        return number == 0 ? 1 : number * Calculate(number - 1);
+    }
+}
---
+++ tests/Algorithms.Tests/Numeric/FactorialTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Wingmann.Algorithms.Numeric;
+
+namespace Wingmann.Algorithms.Tests.Numeric;
+
+public static class FactorialTests
+{
+    [Test]
+    [TestCase(5, 120)]
+    [TestCase(1, 1)]
+    [TestCase(0, 1)]
+    [TestCase(4, 24)]
+    [TestCase(18, 6402373705728000)]
+    [TestCase(10, 3628800)]
+    public static void GetsFactorial(int input, long expected) =>
+        Assert.AreEqual(expected, Factorial.Calculate(input));
+
+    [Test]
+    public static void GetsFactorialExceptionForNonPositiveNumbers([Random(-1000, -1, 10, Distinct = true)] int input)
+        => Assert.Throws<ArgumentException>(() => Factorial.Calculate(input));
+}
